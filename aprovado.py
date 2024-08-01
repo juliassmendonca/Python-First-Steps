b107cd5f1e701c3839cf2734e08a967c20aac60a
Júlia Mendonça <contato.juliasm@gmail.com>
2024-07-31 23:23:47 -0300

    feat:  create  an  exercise with conditional structure

---
+++ aprovado.py
@@ -0,0 +1,11 @@
+nota = 7
+media = 5
+
+input
+
+
+if nota >= media:
+    print("aprovado")
+    
+else: 
+    print("reprovado")
---
+++ colegio-naval.py
@@ -0,0 +1,12 @@
+nota = 4 
+media_alta = 7
+media_baixa = 5
+
+if nota >= media_alta: 
+  print("aprovado direto")
+elif nota >= media_baixa:
+   print("Fará prova final")
+else: 
+   print("fará recuperação final")
+    
+  
