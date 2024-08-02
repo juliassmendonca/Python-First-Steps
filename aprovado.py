b1bc0b11dfe01c63a4a9da43b04c1165de24a922
Júlia Mendonça <contato.juliasm@gmail.com>
2024-08-01 21:21:35 -0300

    feat: transform str into float

--- aprovado.py
+++ aprovado.py
@@ -1,8 +1,6 @@
 nota = 7
 media = 5
 
-input
-
 
 if nota >= media:
     print("aprovado")
---
+++ io.py
@@ -0,0 +1,11 @@
+nota = float(input ("Digite uma nota: "))
+media = float(input ("Digite uma média: "))
+
+if nota >= media:
+    print("aprovado com nota", nota, "(média é",media,")")
+     
+    
+else: 
+    print(f"reprovado com nota {nota}(média é {media})")
+
+
