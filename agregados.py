e80cf0d416ef5f418904ab62c657dc7f7302d167
Júlia Mendonça <contato.juliasm@gmail.com>
2024-08-02 19:48:39 -0300

    feat: doing list DS

---
+++ agregados.py
@@ -0,0 +1,9 @@
+notas = [7.8, 8.2, 5.7]
+disciplinas = ["matematica", "portugeus", "geografia"]
+
+#print (notas, type(notas), type(notas[0]))
+print ( f"As nota da disciplina {disciplinas[0]} foi {notas[0]}.")
+
+disciplinas.append('história')
+print({disciplinas[3]})
+
